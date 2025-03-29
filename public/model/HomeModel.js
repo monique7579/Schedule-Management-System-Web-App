f0660a279242d53f1bde432a8d0a74025a87664d
Sal Bhend <lbhend@uco.edu>
2025-03-29 18:07:32 -0500

    working on layout (cal grid layout)

--- public/model/HomeModel.js
+++ public/model/HomeModel.js
@@ -1,3 +1,19 @@
 export class HomeModel {
-   
+    months = ["January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",];
+
+    today = new Date();
+    month = this.months[this.today.getMonth()];
+
+    
 }
